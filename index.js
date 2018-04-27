8937893e8de3651cf275690300c8c3235c7ca5cb
Greg Allensworth <gregor@greeninfo.org>
2018-04-27 11:23:05 -0700

    timeslider start 1776 + build for demo

--- index.js
+++ index.js
@@ -293,8 +293,8 @@ $(document).ready(function () {
     MAP.addControl(new mapboxgl.NavigationControl());
 
     MAP.TIMESLIDER = new _mbglControlTimeslider.TimeSliderControl({
-        year: 1790,
-        min: 1790,
+        year: 1776,
+        min: 1776,
         max: 2000,
         step: 1,
         play_years: 5,
